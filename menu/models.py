e96660f0e2d59af7cbbb751390229f8f0e369b8b
Abdullah Shahid <abdullah.shahid@enigmatixglobal.com>
2024-12-30 11:44:49 +0500

    image field to blank true

--- menu/models.py
+++ menu/models.py
@@ -12,7 +12,7 @@ class DateTimeAbstractModel(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    image = models.TextField()
+    image = models.TextField(blank=True, null=True)
     count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
